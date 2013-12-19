9702dbd2696a8aac4dba6b0e1df01fa719b37ff8
Jsimmons--dev <Simmons.j@me.com>
2013-12-18 19:29:07 -0500

    Adding combat event System

---
+++ NewGame/Assets/Scripts/Systems/EventSource.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+public class EventSource : MonoBehaviour {
+
+
+	private long timeBetweenHits;
+	private GameObject player;
+	private AttackPacket proxAttackPacket;
+
+	void Start () {
+		player = GameObject.Find("Player");
+		ArrayList<DamagePacket> damageList = new ArrayList<DamagePacket>();
+		damageList.Add(new DamagePacket(DamageType.RANGED,SpecialType.PIERCING,10));
+		proxAttackPacket = new AttackPacket(damageList);
+	}
+
+
+	void FixedUpdate () {
+		if(Vector3.Distance(player.transform.position,this.gameObject.transform.position)<10){
+			player.SendMessage(player.handleAttackPacket(TriggerProxHit));
+		}
+	
+	}
+}
