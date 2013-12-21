7a74f308bb5ed5a7aa5ee5cfb57679fdc7829c68
Jsimmons--dev <Simmons.j@me.com>
2013-12-21 17:49:10 -0500

    starting GUI

--- NewGame/Assets/Scripts/Player/PlayerGUI.cs
+++ NewGame/Assets/Scripts/Player/PlayerGUI.cs
@@ -3,33 +3,67 @@ using System.Collections;
 
 public class PlayerGUI : MonoBehaviour
 {
-	
-		public Texture bloodscreen;
+		//x center of this component of the GUI
+		public int healthX = 0;
+		//y center of this component of the GUI
+		public int healthY = 0;
+		//the width of the component
+		public int healthComponentWidth = 100;
+		//the height of the component
+		public int healthComponentHeight = 100;
 
+		//x center of the minimap
+		public int miniMapX = 0;
+		//y center of the minimap
+		public int miniMapY = 0;
+		// width of the miniMap frame
+		public int miniMapComponentWidth = 100;
+		//the height of the miniMap frame
+		public int miniMapComponentHeight = 100;
+
+	//x center of the skillbar
+	public int skillbarX = 0;
+	//y center of the skillbar
+	public int skillbarY = 0;
+	//width of skillbar component
+	public int skillbarComponentWidth = 100;
+	//height of skillbar component
+	public int skillbarComponentHeight = 100;
+
+		//the healthbar image
+		public Texture healthbar;
+		//the minimap frame image
+		public Texture minimap;
+	//the skillbar image
+		public Texture skillbar;
+
+	void Start(){
+		skillbarX = Screen.width/2;
+		skillbarComponentWidth = Screen.width; 
+		skillbarComponentHeight = Screen.height;
+		skillbarY = Screen.height-64;
+	}
 		void OnGUI ()
 		{	
 				//font size
-				int fontSize = 20;
+				int fontSize = 15;
 
-				GUIStyle style = new GUIStyle ();
+				GUIStyle style = GUI.skin.GetStyle ("Label");
 				style.fontSize = fontSize;
-				GUI.skin.label.alignment = TextAnchor.MiddleCenter;
+				style.alignment = TextAnchor.MiddleCenter;
 
 			
 
-				//x center of this component of the GUI
-				int x = 0;
-				//y center of this component of the GUI
-				int y = 0;
-				//the width of the component
-				int componentWidth = 100;
-				//the height of the component
-				int componentHeight = 100;
-				//draw the background image
-				GUI.DrawTexture (new Rect (x, y, componentWidth, componentHeight), bloodscreen, ScaleMode.ScaleToFit);
-				//draw the players health on top of the background image
-				GUI.Label (new Rect (x +(componentWidth/ 2),y+( componentHeight/2 - fontSize / 2), componentWidth / 2, componentHeight / 2), ((PlayerStats)(GameObject.Find ("Player").GetComponent ("PlayerStats"))).getHealth ().ToString (), style);
 
-		}
+				
+				
+				//draw the background image
+				GUI.DrawTexture (new Rect (healthX - healthComponentWidth / 2, healthY - healthComponentHeight / 2, healthComponentWidth, healthComponentHeight), healthbar, ScaleMode.ScaleToFit);
+				//draw the players health on top of the background image
+				GUI.Label (new Rect (healthX - healthComponentWidth / 2, healthY - healthComponentHeight / 2, healthComponentWidth, healthComponentHeight), ((PlayerStats)(GameObject.Find ("Player").GetComponent ("PlayerStats"))).getHealth ().ToString (), style);
+				//draw minimap
+				GUI.DrawTexture (new Rect (miniMapX - miniMapComponentWidth / 2, miniMapY - miniMapComponentHeight / 2, miniMapComponentWidth, miniMapComponentHeight), minimap, ScaleMode.ScaleToFit);
+		//draw skillbar
+		GUI.DrawTexture (new Rect (skillbarX - skillbarComponentWidth / 2, skillbarY - skillbarComponentHeight / 2, skillbarComponentWidth, skillbarComponentHeight), skillbar, ScaleMode.ScaleToFit);		}
 
 }
